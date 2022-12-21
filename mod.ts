5e08c30fd9efafc5e8ce766257543feee263d200
Canned Tuna <101097344+Canned-Seagull23@users.noreply.github.com>
2022-12-22 00:26:56 +0800

    use a serverStatus namespace for organization

--- mod.ts
+++ mod.ts
@@ -11,31 +11,39 @@ import { commands } from "./commands.ts";
 import { GUILD_ID, DISCORD_TOKEN } from "./config.ts";
 import CONSTANTS, {CONSTANTS_GAMEMODES_TYPE} from "./constants.ts";
 
-let serverStatusRefreshedDate = new Date();
-
-let hosts: any = {};
-let customHosts: any = {};
-let regions: any = {};
-
-async function refreshHosts() {
-    hosts = await fetch(CONSTANTS.DEEEEP_API_URL + "/hosts?servers=1")
-        .then(res => res.json());
-
-    customHosts = await fetch(CONSTANTS.DEEEEP_API_URL + "/hosts?custom=1&servers=1")
-        .then(res => res.json());
-
-    regions = await fetch(CONSTANTS.DEEEEP_API_URL + "/regions")
-        .then(res => res.json());
-
-    hosts = hosts.hosts;
-    customHosts = customHosts.hosts;
-
-    serverStatusRefreshedDate = new Date();
+interface serverStatusInterface {
+    refreshedDate: Date;
+    hosts: any;
+    customHosts: any;
+    regions: any;
+    refreshHosts: Function;
 }
 
-setInterval(refreshHosts, 150000);
+let serverStatus: serverStatusInterface = {
+    refreshedDate: new Date(),
+    hosts: {},
+    customHosts: {},
+    regions: {},
+    refreshHosts: async function() {
+        serverStatus.hosts = await fetch(CONSTANTS.DEEEEP_API_URL + "/hosts?servers=1")
+            .then(res => res.json());
+    
+        serverStatus.customHosts = await fetch(CONSTANTS.DEEEEP_API_URL + "/hosts?custom=1&servers=1")
+            .then(res => res.json());
+    
+        serverStatus.regions = await fetch(CONSTANTS.DEEEEP_API_URL + "/regions")
+            .then(res => res.json());
+    
+        serverStatus.hosts = serverStatus.hosts.hosts;
+        serverStatus.customHosts = serverStatus.customHosts.hosts;
+    
+        serverStatus.refreshedDate = new Date();
+    }
+}
 
-refreshHosts()
+setInterval(() => serverStatus.refreshHosts(), 150000);
+
+serverStatus.refreshHosts()
 
 class TagBot extends Client {
     @event()
@@ -63,22 +71,22 @@ class TagBot extends Client {
         let regularServerList = "";
         let customServerList = "";
 
-        for (const i in hosts) {
+        for (const i in serverStatus.hosts) {
             regularServerList += `
-${CONSTANTS.GAMEMODES[(String(hosts[i]!.gamemode) as CONSTANTS_GAMEMODES_TYPE)]} - ${hosts[i].server.region} - ${hosts[i].id} - ${hosts[i].map.string_id} - ${hosts[i].users} players
+${CONSTANTS.GAMEMODES[(String(serverStatus.hosts[i]!.gamemode) as CONSTANTS_GAMEMODES_TYPE)]} - ${serverStatus.hosts[i].server.region} - ${serverStatus.hosts[i].id} - ${serverStatus.hosts[i].map.string_id} - ${serverStatus.hosts[i].users} players
             `;
         }
 
-        for (const i in customHosts) {
+        for (const i in serverStatus.customHosts) {
             customServerList += `
-${CONSTANTS.GAMEMODES[(String(customHosts[i]!.gamemode) as CONSTANTS_GAMEMODES_TYPE)]} - ${customHosts[i].server.region} - ${customHosts[i].id} - ${customHosts[i].map.string_id} - ${hosts[i].users} players
+${CONSTANTS.GAMEMODES[(String(serverStatus.customHosts[i]!.gamemode) as CONSTANTS_GAMEMODES_TYPE)]} - ${serverStatus.customHosts[i].server.region} - ${serverStatus.customHosts[i].id} - ${serverStatus.customHosts[i].map.string_id} - ${serverStatus.customHosts[i].users} players
             `;
         }
 
         i.respond({
             content: `
 **Deeeep.io Server Status**
-(Last refreshed at ${serverStatusRefreshedDate.getUTCHours()}:${serverStatusRefreshedDate.getUTCMinutes()}:${serverStatusRefreshedDate.getUTCSeconds()} UTC)
+(Last refreshed at ${serverStatus.refreshedDate.getUTCHours()}:${serverStatus.refreshedDate.getUTCMinutes()}:${serverStatus.refreshedDate.getUTCSeconds()} UTC)
 
 **Regular Servers**
 \`\`\`yaml
